de1bdc3d3b91df9a0f4bdef422e2ba540c08251a
John Nelson <jrnelson75032@gmail.com>
2024-09-09 07:45:43 -0500

    Imports Route, BrowserRouter, and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 const App = () => {
   return (
     <h1 className="text-3xl font-bold underline">
