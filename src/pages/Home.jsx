fd18f085145467c44bf10257402427284c29026f
John Nelson <jrnelson75032@gmail.com>
2024-09-16 07:46:00 -0500

    Implements sky scene

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
 import Island from '../models/Island';
+import Sky from '../models/Sky';
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
@@ -28,10 +29,10 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <directionalLight position={[1, 1, 1]} intensity={3} />
           <ambientLight intensity={0.5} />
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
-
+          <Sky />
           <Island 
             position={islandPosition}
             scale={islandScale}
