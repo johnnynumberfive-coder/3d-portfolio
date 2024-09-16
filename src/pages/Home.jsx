b46fd1de0d78428ac5ca14ab6f18b03be0ca8e77
John Nelson <jrnelson75032@gmail.com>
2024-09-16 07:07:34 -0500

    Configures hemisphere light

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1, 1, 1]} intensity={2} />
           <ambientLight intensity={0.5} />
-          <hemisphereLight />
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
           <Island 
             position={islandPosition}
